f5efa1475fd3d8735261524299a857c4bf0a1118
tmmluis <tmmluis@gmail.com>
2023-03-28 14:28:27 +0200

    add current date to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,19 @@ import { Flex, Spacer, Center, Image } from '@chakra-ui/react';
 import headerLogo from '../assets/header_logo.png';
 
 export function Header() {
+  const currentDate = new Date(Date.now());
+  const options = { dateStyle: 'full' } as const;
+  const currentTime = currentDate.toTimeString().split(' ')[0].slice(0, 5);
+
   return (
     <Flex as="header" p="40px">
       <Center pl="16px">
         <Image src={headerLogo} alt="WeatherNow logo" />
       </Center>
       <Spacer />
-      <Center pr="16px">Friday, 24 March, 2023 | 10:33</Center>
+      <Center pr="16px">
+        {currentDate.toLocaleDateString(undefined, options)} | {currentTime}
+      </Center>
     </Flex>
   );
 }
