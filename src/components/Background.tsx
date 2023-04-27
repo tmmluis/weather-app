150fd44ab83fb1785542e07cb6263334ab38b3b0
tmmluis <tmmluis@gmail.com>
2023-04-27 12:55:08 +0200

    add simple background animation

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -2,6 +2,7 @@ import { Flex, Box, Image } from '@chakra-ui/react';
 import { Tag } from './Tag';
 import { City, getCity } from '../util/cities';
 import { useEffect, useState } from 'react';
+import './backround.css';
 
 type BackgroundProps = {
   isPhoto: boolean;
@@ -11,23 +12,32 @@ export function Background({ isPhoto }: BackgroundProps) {
   const [city, setCity] = useState<City>(getCity());
 
   useEffect(() => {
-    const interval = setInterval(() => setCity(getCity()), 3000);
-    return () => clearInterval(interval);
-  }, []);
+    if (isPhoto) {
+      const interval = setInterval(() => setCity(getCity()), 3000);
+      return () => clearInterval(interval);
+    }
+  }, [isPhoto]);
 
   return (
     <>
-      <Flex pos="fixed" bottom="0" w="100%" height="55%" zIndex="-1">
-        {isPhoto ? (
-          <Image src={city.image} flexGrow="1" objectFit="cover" />
-        ) : (
-          <Box flexGrow="1" backgroundColor="blue.50" />
-        )}
-      </Flex>
-      {isPhoto && (
-        <Box pos="fixed" bottom="0" left="0">
-          <Tag city={city.name} country={city.country} />
+      {isPhoto ? (
+        <Box key={city.name} className="fade-in">
+          <Flex pos="fixed" bottom="0" w="100%" height="55%" zIndex="-1">
+            <Image src={city.image} flexGrow="1" objectFit="cover" />
+          </Flex>
+          <Box pos="fixed" bottom="0" left="0">
+            <Tag city={city.name} country={city.country} />
+          </Box>
         </Box>
+      ) : (
+        <Box
+          pos="fixed"
+          bottom="0"
+          w="100%"
+          height="55%"
+          zIndex="-1"
+          backgroundColor="blue.50"
+        />
       )}
     </>
   );
