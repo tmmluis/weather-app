b67003208a38fb8df1bfcd93c4bd78794f8cb343
tmmluis <tmmluis@gmail.com>
2023-05-02 16:00:48 +0200

    fix locations list padding top

--- src/components/LocationsList.tsx
+++ src/components/LocationsList.tsx
@@ -8,7 +8,7 @@ type LocationsListProps = {
 
 export function LocationsList({ locations }: LocationsListProps) {
   return (
-    <Box zIndex={1} pt="88px">
+    <Box zIndex={1} pt={{ base: '68px', md: '88px' }}>
       {locations.map((location: LocationData) => (
         <WeatherCard
           location={location}
